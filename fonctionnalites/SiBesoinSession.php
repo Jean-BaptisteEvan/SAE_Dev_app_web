beb889e559e5aebcf21daf05fe85be7762405655
GEORGES Vincent <vincent.georges3@etu.univ-lorraine.fr>
2023-11-08 18:07:02 +0100

    ajout de la fonctionnalité de note avec un getMoyenne et modif de répartition

--- fonctionnalites/SiBesoinSession.php
+++ fonctionnalites/SiBesoinSession.php
@@ -2,9 +2,11 @@
 require_once "./fonctionnalites_6_et_7.php";
 require_once "./fonctionnalite_10.php";
 require_once "./fonctionnalite_13_et_14.php";
+require_once "./note.php";
 use iutnc\fonctionnalites\connexion;
 use iutnc\fonctionnalites\supression;
 use iutnc\fonctionnalites\follow;
+use iutnc\fonctionnalites\note;
 
 echo '<a href="?action=create">Créer compte</a>'."<br>";
 echo '<a href="?action=connect">Se connecter</a>'."<br>";
@@ -12,6 +14,8 @@ echo '<a href="?action=deconnect">Se déconnecter</a>'."<br>";
 echo '<a href="?action=delete">Suprimer touite</a>'."<br>";
 echo '<a href="?action=followuser">Suivre un utilisateur</a>'."<br>";
 echo '<a href="?action=followtag">Suivre un tag</a>'."<br>";
+echo '<a href="?action=noter">Noter un touite</a>'."<br>";
+echo '<a href="?action=note">Voir la moyenne d un touite</a>'."<br>";
 
 if(isset($_GET['action']) and $_GET['action']==="create"){
     connexion::creationCompte();
@@ -30,6 +34,12 @@ if(isset($_GET['action']) and $_GET['action']==="followuser"){
 }
 if(isset($_GET['action']) and $_GET['action']==="followtag"){
     follow::followTag();
+}
+if(isset($_GET['action']) and $_GET['action']==="noter"){
+    note::noter();
+}
+if(isset($_GET['action']) and $_GET['action']==="note"){
+    echo(note::getMoyenne());
 }
 
 ?>
