e7045e9672b688bbbb2e788c95e730d7b87d87fb
Corentin <corentin.froger@netc.fr>
2023-11-08 15:19:45 +0100

    Ajout fonctionnalité 3 dans le main (changer manuellement le code) + Amélioration TouiteRenderer et css pour touites

--- php/TouiteRenderer.php
+++ php/TouiteRenderer.php
@@ -14,9 +14,12 @@ class TouiteRenderer {
      * @param Touite $touite the touite we want to display
      * @return string the string displaying the touite
      */
-    public static  function renderCourt(Touite $touite) : string {
-        return "par {$touite->getPosteur()} le {$touite->getDate()}<br>
-                <dd>{$touite->getTexte()}</dd><br>";
+    public static function renderCourt(Touite $touite) : string {
+        return "<ul>par <a id='posteur' href='https://fr.wikipedia.org/wiki/Chat#/media/Fichier:Collage_of_Six_Cats-02.jpg'>{$touite->getPosteur()}</a>
+                <dd>{$touite->getTexte()}</dd>
+                <br>
+                <dd>{$touite->getDate()}</dd>
+                </ul>";
     }
 
     /**
@@ -24,18 +27,18 @@ class TouiteRenderer {
      * @param Touite $touite the touite we want to display
      * @return string the string displaying the touite
      */
-    public static  function renderLong(Touite $touite) : string {
+    public static function renderLong(Touite $touite) : string {
         // Add the tags
         $repTags = "";
         foreach ($touite->getListeTags() as $k => $v) {
             $repTags .= "#{$v->getLibelle()} ";
         }
 
-        $rep = "par {$touite->getPosteur()} le {$touite->getDate()}<br>
-                <dd>$repTags<br>{$touite->getTexte()}</dd>
+        return "<ul>par <a id='posteur' href='https://fr.wikipedia.org/wiki/Chat#/media/Fichier:Collage_of_Six_Cats-02.jpg'>{$touite->getPosteur()}</a>
+                <dd><a id='tag' href='https://fr.wikipedia.org/wiki/Chat#/media/Fichier:Collage_of_Six_Cats-02.jpg'>$repTags</a> {$touite->getTexte()}</dd>
                 <dd>{$touite->getImage()->getChemin()}</dd>
-                <dd>{$touite->getNote()}</dd>
-                <br>";
-        return $rep;
+                <br>
+                <dd>{$touite->getDate()} note : {$touite->getNote()}
+                </ul>";
     }
 }
