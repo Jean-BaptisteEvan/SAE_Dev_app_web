dfb35ea10f3ae9c979e5ab195f77f856497d9c63
Corentin <corentin.froger@netc.fr>
2023-11-10 10:11:51 +0100

    Combined duplicated code into a single function

--- php/TouiteSearch.php
+++ php/TouiteSearch.php
@@ -6,6 +6,8 @@ require_once '../vendor/autoload.php';
 
 use iutnc\touiteur\bdd;
 use iutnc\touiteur;
+use PDO;
+use PDOStatement;
 
 /**
  * Class used to search touites, e.g : every touite posted by someone
@@ -14,26 +16,23 @@ use iutnc\touiteur;
 class TouiteSearch {
 
     /**
-     * Return every touites
+     * Creates an array of touites depending of the request in parameter
+     * specifically meant to shorten code length
+     * @param PDO $bdd the connection to the database
+     * @param PDOStatement $requete the prepared SQL request
      * @return array the array containing the touites
      */
-    public static function getAllTouites() : array {
-        bdd\ConnectionFactory::makeConnection();
-        $bdd = bdd\ConnectionFactory::$bdd;
-
+    public static function creerListeTouites(PDO $bdd, PDOStatement $requete) : array {
         $listeTouites = array();
 
-        $requete1 = $bdd->prepare("SELECT Touite.datePublication, User.pseudo, Touite.texte, Touite.idTouite FROM Touite
-                                            INNER JOIN User ON User.idUser = Touite.idUser
-                                            ORDER BY datePublication DESC");
-        $requete1->execute();
-        while($ligne1=$requete1->fetch()) {
+        $requete->execute();
+        while($ligne1=$requete->fetch()) {
             $format = "l j F Y h:i:s";
             $dateBrute = explode("-", $ligne1[0]);
             $heureComplete = explode(":", explode(" ", $dateBrute[2])[1]);
 
             $date = mktime($heureComplete[0], $heureComplete[0], $heureComplete[0],
-                           $dateBrute[1], explode(" ", $dateBrute[2])[0], $dateBrute[0]);
+                $dateBrute[1], explode(" ", $dateBrute[2])[0], $dateBrute[0]);
 
             $date = date($format, $date);
 
@@ -55,9 +54,24 @@ class TouiteSearch {
 
             array_push($listeTouites, $touite);
         }
-        $requete1->closeCursor();
+        $requete->closeCursor();
 
         return $listeTouites;
+    }
+
+    /**
+     * Return every single touite
+     * @return array the array containing the touites
+     */
+    public static function getAllTouites() : array {
+        bdd\ConnectionFactory::makeConnection();
+        $bdd = bdd\ConnectionFactory::$bdd;
+
+        $requete = $bdd->prepare("SELECT Touite.datePublication, User.pseudo, Touite.texte, Touite.idTouite FROM Touite
+                                            INNER JOIN User ON User.idUser = Touite.idUser
+                                            ORDER BY datePublication DESC");
+
+        return self::creerListeTouites($bdd, $requete);
     }
 
     /**
@@ -69,44 +83,11 @@ class TouiteSearch {
         bdd\ConnectionFactory::makeConnection();
         $bdd = bdd\ConnectionFactory::$bdd;
 
-        $listeTouites = array();
-
-        $requete1 = $bdd->prepare("SELECT Touite.datePublication, User.pseudo, Touite.texte, Touite.idTouite FROM Touite
+        $requete = $bdd->prepare("SELECT Touite.datePublication, User.pseudo, Touite.texte, Touite.idTouite FROM Touite
                                             INNER JOIN User ON User.idUser = Touite.idUser
                                             WHERE Touite.idUser = ? ORDER BY datePublication DESC");
-        $requete1->bindParam(1, $idUser);
-        $requete1->execute();
-        while($ligne1=$requete1->fetch()) {
-            $format = "l j F Y h:i:s";
-            $dateBrute = explode("-", $ligne1[0]);
-            $heureComplete = explode(":", explode(" ", $dateBrute[2])[1]);
-
-            $date = mktime($heureComplete[0], $heureComplete[0], $heureComplete[0],
-                $dateBrute[1], explode(" ", $dateBrute[2])[0], $dateBrute[0]);
-
-            $date = date($format, $date);
-
-            $touite = new touiteur\Touite($date, $ligne1[1], $ligne1[2]);
-
-            // Add the tags related to this touite
-            $requete2 = $bdd->prepare("SELECT Tag.tagLibelle, Tag.tagDesc, Image.chemin, Image.imgDesc FROM Tag
-	                                            INNER JOIN TagJoint ON TagJoint.idTag = Tag.idTag
-                                                INNER JOIN imagejointe ON imagejointe.idTouite = tagjoint.idTouite
-	                                            INNER JOIN Image ON image.idImage = imagejointe.idimage
-  	                                            WHERE TagJoint.idTouite = ?");
-            $requete2->bindParam(1, $ligne1[3]);
-            $requete2->execute();
-            while($ligne2=$requete2->fetch()) {
-                $touite->ajouterTag(new Tag($ligne2[0], $ligne2[1]));
-                $touite->ajouterImage($ligne2[2], $ligne2[3]);
-            }
-            $requete2->closeCursor();
-
-            array_push($listeTouites, $touite);
-        }
-        $requete1->closeCursor();
-        
-        return $listeTouites;
+        $requete->bindParam(1, $idUser);
+        return self::creerListeTouites($bdd, $requete);
     }
 
     /**
@@ -118,45 +99,12 @@ class TouiteSearch {
         bdd\ConnectionFactory::makeConnection();
         $bdd = bdd\ConnectionFactory::$bdd;
 
-        $listeTouites = array();
-
-        $requete1 = $bdd->prepare("SELECT Touite.datePublication, User.pseudo, Touite.texte, Touite.idTouite FROM Touite
+        $requete = $bdd->prepare("SELECT Touite.datePublication, User.pseudo, Touite.texte, Touite.idTouite FROM Touite
                                             INNER JOIN User ON User.idUser = Touite.idUser
                                             INNER JOIN TagJoint ON TagJoint.idTouite = Touite.idTouite
                                             INNER JOIN Tag ON Tag.idTag = TagJoint.idTag
                                             WHERE Tag.tagLibelle = ? ORDER BY datePublication DESC");
-        $requete1->bindParam(1, $tagLibelle);
-        $requete1->execute();
-        while($ligne1=$requete1->fetch()) {
-            $format = "l j F Y h:i:s";
-            $dateBrute = explode("-", $ligne1[0]);
-            $heureComplete = explode(":", explode(" ", $dateBrute[2])[1]);
-
-            $date = mktime($heureComplete[0], $heureComplete[0], $heureComplete[0],
-                $dateBrute[1], explode(" ", $dateBrute[2])[0], $dateBrute[0]);
-
-            $date = date($format, $date);
-
-            $touite = new touiteur\Touite($date, $ligne1[1], $ligne1[2]);
-
-            // Add the tags related to this touite
-            $requete2 = $bdd->prepare("SELECT Tag.tagLibelle, Tag.tagDesc, Image.chemin, Image.imgDesc FROM Tag
-	                                            INNER JOIN TagJoint ON TagJoint.idTag = Tag.idTag
-                                                INNER JOIN imagejointe ON imagejointe.idTouite = tagjoint.idTouite
-	                                            INNER JOIN Image ON image.idImage = imagejointe.idimage
-  	                                            WHERE TagJoint.idTouite = ?");
-            $requete2->bindParam(1, $ligne1[3]);
-            $requete2->execute();
-            while($ligne2=$requete2->fetch()) {
-                $touite->ajouterTag(new Tag($ligne2[0], $ligne2[1]));
-                $touite->ajouterImage($ligne2[2], $ligne2[3]);
-            }
-            $requete2->closeCursor();
-
-            array_push($listeTouites, $touite);
-        }
-        $requete1->closeCursor();
-
-        return $listeTouites;
+        $requete->bindParam(1, $tagLibelle);
+        return self::creerListeTouites($bdd, $requete);
     }
 }
