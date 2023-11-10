84a354ec812e0d5edbe8504026ae80a8b0411b51
Jean-Baptiste Evan~ <evanjeanbaptiste@gmail.com>
2023-11-10 17:49:17 +0100

    renommage de SignUp en Disconnect

--- php/Dispacheur.php
+++ php/Dispacheur.php
@@ -35,7 +35,7 @@
             <li><div class="container"><a href="DispacheurProf.php"><img src="Image/user-removebg-preview.png " class="imgheaderhome"></a><h3>Profile</h3></div></li>
             <li><div class="contain"><a href="Post.php" class="imgPost"><h4>Post</h4></a></div> </li>
             <li><div class="container"><a href="Connection.php" ><img src="Image/door-green.png"></a><h3>SignIn</h3></div> </li>
-            <li><div class="container"><a href="MainDeconnection.php" ><img src="Image/door-Red.png"></a><h3>SignUp</h3></div> </li>
+            <li><div class="container"><a href="MainDeconnection.php" ><img src="Image/door-Red.png"></a><h3>Disconnect</h3></div> </li>
         </ul>
     </nav>
 
