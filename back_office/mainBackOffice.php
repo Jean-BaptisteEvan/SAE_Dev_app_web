0f604a0c57ca99e27643e58b01aa7ce031786830
Jean-Baptiste Evan~ <evanjeanbaptiste@gmail.com>
2023-11-10 10:28:46 +0100

    modification du back office pour coller aux nouvelles classes

--- back_office/mainBackOffice.php
+++ back_office/mainBackOffice.php
@@ -3,18 +3,32 @@ require_once '../vendor/autoload.php';
 
 use iutnc\touiteur\bdd\ConnectionFactory;
 require_once './gestionBackOffice.php';
-require_once '../fonctionnalites/fonctionnalites_6_et_7.php';
-use iutnc\fonctionnalites\connexion;
-try {
-    connexion::connexion();
-} catch (Exception $e) {
+use iutnc\touiteur\Compte;
 
+
+ConnectionFactory::makeConnection();
+$bdd = ConnectionFactory::$bdd;
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    //formulaire de connexion
+    echo'<form action="">
+            <input type="text" name="pseudo" placeholder="login">
+            <input type="password" name="mdp" placeholder="password">
+            <input type="submit" class="fadeIn fourth" value="Log In">
+        </form>';
+} else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    try {
+        Compte::connexion($_POST['pseudo'], $_POST['$mdp']);
+    } catch (Exception $e) {
+
+    }
 }
+
 if(isset($_SESSION['user'])){
     if($_SESSION['user']['admin'] == 1){
-        ConnectionFactory::makeConnection();
-        $bdd = ConnectionFactory::$bdd;
+
         gestionBackOffice::genererBackOffice($bdd);
+
     }else{
         echo 'access not granted please leave';
     }
